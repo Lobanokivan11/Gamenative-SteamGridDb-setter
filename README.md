d2391fc1774c5dad4f5bfdf95a7664afbfb55b32
Lobanokivan11 <lobanokivan11@gmail.com>
2026-02-03 14:20:40 +0300

    INITIAL

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Gamenative-SteamGridDb-setter
+
+## How to use
+
+1. get SteamGridDb Key from (link)[https://www.steamgriddb.com/profile/preferences]
+
+2. Insert This Key to /sdcard/steamgriddb_key.txt file (Create This file if there isn't)
+
+3. install this apk and activate either LSPOSED (Zygisk) or LSPATCH (rootless but requires Gamenative patch via Lspatch)
+
+
---
+++ app/src/main/java/io/github/lobanokivan11/gamenativegrid/HOOK.java
@@ -0,0 +1,45 @@
+package io.github.lobanokivan11.gamenativegrid;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+import java.security.KeyStore;
+import java.util.Arrays;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+@SuppressLint("DiscouragedPrivateApi")
+@SuppressWarnings("ConstantConditions")
+public class HOOK implements IXposedHookLoadPackage {
+
+    private static final String TAG = HOOK.class.getSimpleName();
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+	    if (!lpparam.packageName.equals("app.gamenative")) return;
+	    try {
+        	Class<?> buildConfigClass = XposedHelpers.findClass(
+         		lpparam.packageName + ".BuildConfig", 
+        	    lpparam.classLoader
+  	        );
+            BufferedReader br = new BufferedReader(new FileReader("/sdcard/steamgriddb_key.txt"));
+			String secretKey = br.readLine();
+			br.close();
+			if (secretKey != null && !secretKey.isEmpty()) {
+				XposedHelpers.setStaticObjectField(buildConfigClass, "STEAMGRIDDB_API_KEY", secretKey.trim());
+			}
+	    } catch (XposedHelpers.ClassNotFoundError e) {
+	        XposedBridge.log("BuildConfig class not found for " + lpparam.packageName);
+	    } catch (IOException e) {
+	        XposedBridge.log("Error on Reading Api Key! Please Check /sdcard/steamgriddb_key.txt file for mistakes" + e.getMessage());
+	    } catch (NoSuchFieldError e) {
+	        XposedBridge.log("Field not found in BuildConfig");
+	    }
+	}
+}
